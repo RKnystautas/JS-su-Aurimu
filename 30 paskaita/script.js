eba31730a8427ab1ca68e718cde93f083624430b
Raimondas Knystautas <email@com.com>
2022-01-20 19:47:19 +0200

    JS alert task

--- 30 paskaita/script.js
+++ 30 paskaita/script.js
@@ -3,5 +3,11 @@
 // alert('pirmas alertas');
 // alert('antras alertas');
 
-let budget = 1000
-let productPrice = 1700
+let budget = 1000;
+let productPrice = 1700;
+
+if (budget > productPrice) {
+    alert("You have enough money to buy IT!")
+} else {
+    alert("not enough money in your wallet...")
+}
