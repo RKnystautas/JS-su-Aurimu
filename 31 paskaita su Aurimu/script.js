ea35bc9bb9af708b6bc24280c07d88d1702327f0
Raimondas Knystautas <email@com.com>
2022-01-22 14:45:09 +0200

    eur to rub converter

--- 31 paskaita su Aurimu/script.js
+++ 31 paskaita su Aurimu/script.js
@@ -6,10 +6,24 @@ Funkcija atsakyma grazina;
 Is pakviestos funkcijos grazinta atsakyma mes alertinam;
 */
 
-function suma(a, b) {
-    return a + b;
-}
+// function suma(a, b) {
+//     return a + b;
+// }
 
-const rezultatas = suma(7, 12);
-alert(rezultatas);
-// alert(suma(1, 2));
+// const rezultatas = suma(7, 12);
+// alert(rezultatas);
+// // alert(suma(1, 2));
+
+
+
+// Eur to RUB converter
+
+const eur = prompt('Iveskite kieki EUR :');
+
+const atsakymas = eurToRub(eur);
+
+alert(atsakymas);
+
+function eurToRub(eur) {
+    return eur * 88.05;
+}
