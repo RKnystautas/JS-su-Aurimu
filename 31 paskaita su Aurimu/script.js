937e2297f368e933b346f4938ddfdadd9c27eec0
Raimondas Knystautas <email@com.com>
2022-01-22 11:33:45 +0200

    uzduotis is skaidriu, funkcijos kurimas

---
+++ 31 paskaita su Aurimu/script.js
@@ -0,0 +1,42 @@
+// function sunkusisSestadienis(parametras) {
+
+//     return parametras - 1;
+// }
+
+
+// const kibiras = sunkusisSestadienis(6);
+
+
+
+// console.log(kibiras);
+
+// // console.log(sunkusisSestadienis());
+
+// function vardoPavardesGeneratorius(name, surname) {
+//     return name + ' ' + surname;
+// }
+
+// const vardas = 'aurimas';
+// const pavarde = 'stecenka';
+
+// const egidijausVardas = 'egidijus';
+// const egidijausPavarde = 'kivilis';
+
+// const manoPilnasVardas = vardoPavardesGeneratorius('aurimas', 'stecenka');
+// const egidijausPilnasVardas = vardoPavardesGeneratorius('egidijus', 'kivilis');
+
+// console.log
+
+function min(a, b) {
+    console.log('veikia')
+
+    if (a < b) {
+        return a;
+    }
+    return b;
+
+
+}
+
+let number = min(1, 1);
+console.log(number);
